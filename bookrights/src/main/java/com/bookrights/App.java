272eb0fd60d82093d6acd615c4524862122996e3
Nur Atto <kobe11nur@gmail.com>
2026-01-17 19:47:41 -0600

    Configure Spring Boot App

--- bookrights/src/main/java/com/bookrights/App.java
+++ bookrights/src/main/java/com/bookrights/App.java
@@ -1,10 +1,12 @@
 package com.bookrights;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Marks this as the main Spring Boot application
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args); // Starts the Spring Boot app
     }
 }
